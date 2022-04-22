f786c150426d3dc5a8478000a662c7ba6e0bff32
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2022-04-22 14:39:04 +0900

    fix: Change to img tags

--- components/atoms/Picture.js
+++ components/atoms/Picture.js
@@ -1,12 +1,11 @@
 import { memo } from "react";
-import Image from "next/image";
 
 export const Picture = memo(function Picture({ base, md, lg }) {
   return (
     <picture>
       <source width='100%' srcSet={lg} media='(min-width: 1000px)' />
       <source width='100%' srcSet={md} media='(min-width: 670px)' />
-      <Image width='100%' src={base} alt='test' />
+      <img width='100%' src={base} alt='test' />
     </picture>
   );
 });
